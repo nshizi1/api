0d84bbeafb23fe3fc2dcd3047238741a7ece49a9
Wilson Nshizirungu <wilsonnshi1@gmail.com>
2024-03-26 03:59:01 +0200

    add new folder

---
+++ script.js
@@ -0,0 +1,8 @@
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then((res) => res.json())
+// .then(data => {
+//     console.log(data);
+// })
+setTimeout(function() {
+    console.log("This message will appear after 2000 milliseconds (2 seconds)");
+}, 2000);
